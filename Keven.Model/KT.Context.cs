df9805343b4cc1c0938f4c666422f8812d8ac1aa
Minas <nestax@qq.com>
2019-03-19 14:53:45 +0800

    修改了model

--- Keven.Model/KT.Context.cs
+++ Keven.Model/KT.Context.cs
@@ -31,18 +31,6 @@ namespace Keven.Model
         public virtual DbSet<SYS_FUNCTION> SYS_FUNCTION { get; set; }
         public virtual DbSet<SYS_ORGANIZATION> SYS_ORGANIZATION { get; set; }
         public virtual DbSet<SYS_PART_FUNC> SYS_PART_FUNC { get; set; }
-        public virtual DbSet<TT_Advertising> TT_Advertising { get; set; }
-        public virtual DbSet<TT_ApiLog> TT_ApiLog { get; set; }
-        public virtual DbSet<TT_Finance> TT_Finance { get; set; }
-        public virtual DbSet<TT_Kefu> TT_Kefu { get; set; }
-        public virtual DbSet<TT_Message> TT_Message { get; set; }
-        public virtual DbSet<TT_Sms> TT_Sms { get; set; }
-        public virtual DbSet<TT_StateLog> TT_StateLog { get; set; }
-        public virtual DbSet<TT_Trademark> TT_Trademark { get; set; }
-        public virtual DbSet<TT_Trademark_Applyer> TT_Trademark_Applyer { get; set; }
-        public virtual DbSet<TT_Trademark_Class> TT_Trademark_Class { get; set; }
-        public virtual DbSet<TT_Trademark_Files> TT_Trademark_Files { get; set; }
-        public virtual DbSet<TT_User> TT_User { get; set; }
         public virtual DbSet<UR_PART> UR_PART { get; set; }
         public virtual DbSet<UR_USERS> UR_USERS { get; set; }
     }
